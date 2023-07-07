216525d72e65881409186f7c32eee201c61efc23
lakshmineha <srilaxmi550@gmail.com>
2023-07-07 16:05:27 +0530

    first

---
+++ src/banking/add.java
@@ -0,0 +1,14 @@
+package banking;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("print c value" +c);
+		
+	}
+
+}
